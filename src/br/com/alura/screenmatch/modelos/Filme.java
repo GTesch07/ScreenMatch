a0a924e0434bafbc06f46372c886dc1106f0eacb
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-05-31 23:34:22 -0300

    Adicionando uma interface e utilizando o implements para implementar a interface no arquivo Principal.

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -2,8 +2,11 @@ package br.com.alura.screenmatch.modelos;
 
 //Definição de uma Classe, tudo o que uma classe tem e tudo o que uma classe faz!
 
+import br.com.alura.screenmatch.calculos.Classificavel;
+
 //extends = Palavra reservada do Java que aplica herança a uma classe)
-public class Filme extends Titulo{
+//Implements = Palavra reservada do Java que aplica ainda tem a herança da classe do extends mas implementa a classe classificavel.
+public class Filme extends Titulo implements Classificavel {
     private String diretor;
 
     public String getDiretor(){
@@ -12,5 +15,10 @@ public class Filme extends Titulo{
 
     public void setDiretor(String diretor){
         this.diretor = diretor;
+    }
+
+    @Override
+    public int getClassificacao(){
+        return 0;
     }
 }
